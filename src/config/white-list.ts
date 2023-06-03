305ea906b4ae8bf98174b9a8bbf8962a13f53cda
leyi <leyi@myun.info>
2023-06-03 15:40:34 +0800

    fix bug

--- src/config/white-list.ts
+++ src/config/white-list.ts
@@ -1,6 +1,16 @@
+/*
+ * @Author: leyi leyi@myun.info
+ * @Date: 2023-03-10 02:34:58
+ * @LastEditors: leyi leyi@myun.info
+ * @LastEditTime: 2023-06-03 15:40:21
+ * @FilePath: /easy-front-gpt-api/src/config/white-list.ts
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('while_list', () => ({
-  token: ['/api/oss/upload-oss'],
+  token: ['/api/oss/upload-oss', '/api/user/login', '/api/user/get-session'],
   sign: ['/api/oss/upload-oss'],
 }));
